1dc29bd8a7b84b9b46fe2362a017bbbb076c1b42
Andre Joseph Cubeta <ajcubeta@gmail.com>
2011-12-01 15:26:08 +0800

    Save changes on generate post model for backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f21bb00874b477dc838f6bc6b0d491351d948eec2ebb5567fa0b348ce93eeec9d97cdaba739596dac8981f4c54fa2ed84adb4855cd8d5d8bd378d845df0c17b4'
